cfd4bd22220c41c17b7cf4fb2e5eca00655cf167
KELVIN <kelvinkipyegon568@gmail.com>
2025-11-11 17:13:37 +0300

    Add timestamp fields (created_at, updated_at) to Recipe model

--- recipe/models.py
+++ recipe/models.py
@@ -9,7 +9,10 @@ class Recipe(models.Model):
     equipment = models.TextField()
     instructions = models.TextField()
     tips = models.TextField
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
 
     def __str__(self):
         return f" Title: {self.title}"
